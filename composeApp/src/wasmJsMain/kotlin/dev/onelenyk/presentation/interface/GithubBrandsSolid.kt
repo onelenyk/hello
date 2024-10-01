b6a511117a8ff2e0914a3e66725730ca32b82418
Nazar Lenyk <nazar.lenyk@vanongo.com>
2024-10-01 23:51:29 +0300

    feat: create MainActivity and GithubBrandsSolid classes

---
+++ composeApp/src/wasmJsMain/kotlin/dev/onelenyk/presentation/interface/GithubBrandsSolid.kt
@@ -0,0 +1,111 @@
+package dev.onelenyk.presentation.`interface`/*
+* Converted using https://composables.com/svgtocompose
+*/
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+public val GithubBrandsSolid: ImageVector
+	get() {
+		if (_GithubBrandsSolid != null) {
+			return _GithubBrandsSolid!!
+		}
+		_GithubBrandsSolid = ImageVector.Builder(
+            name = "GithubBrandsSolid",
+            defaultWidth = 496.dp,
+            defaultHeight = 512.dp,
+            viewportWidth = 496f,
+            viewportHeight = 512f
+        ).apply {
+			path(
+    			fill = SolidColor(Color.Black),
+    			fillAlpha = 1.0f,
+    			stroke = null,
+    			strokeAlpha = 1.0f,
+    			strokeLineWidth = 1.0f,
+    			strokeLineCap = StrokeCap.Butt,
+    			strokeLineJoin = StrokeJoin.Miter,
+    			strokeLineMiter = 1.0f,
+    			pathFillType = PathFillType.NonZero
+			) {
+				moveTo(165.9f, 397.4f)
+				curveToRelative(00f, 20f, -2.30f, 3.60f, -5.20f, 3.60f)
+				curveToRelative(-3.30f, 0.30f, -5.60f, -1.30f, -5.60f, -3.60f)
+				curveToRelative(00f, -20f, 2.30f, -3.60f, 5.20f, -3.60f)
+				curveToRelative(30f, -0.30f, 5.60f, 1.30f, 5.60f, 3.60f)
+				close()
+				moveToRelative(-31.1f, -4.5f)
+				curveToRelative(-0.70f, 20f, 1.30f, 4.30f, 4.30f, 4.90f)
+				curveToRelative(2.60f, 10f, 5.60f, 00f, 6.20f, -20f)
+				reflectiveCurveToRelative(-1.3f, -4.3f, -4.3f, -5.2f)
+				curveToRelative(-2.60f, -0.70f, -5.50f, 0.30f, -6.20f, 2.30f)
+				close()
+				moveToRelative(44.2f, -1.7f)
+				curveToRelative(-2.90f, 0.70f, -4.90f, 2.60f, -4.60f, 4.90f)
+				curveToRelative(0.30f, 20f, 2.90f, 3.30f, 5.90f, 2.60f)
+				curveToRelative(2.90f, -0.70f, 4.90f, -2.60f, 4.60f, -4.60f)
+				curveToRelative(-0.30f, -1.90f, -30f, -3.20f, -5.90f, -2.90f)
+				close()
+				moveTo(244.8f, 8f)
+				curveTo(106.10f, 80f, 00f, 113.30f, 00f, 2520f)
+				curveToRelative(00f, 110.90f, 69.80f, 205.80f, 169.50f, 239.20f)
+				curveToRelative(12.80f, 2.30f, 17.30f, -5.60f, 17.30f, -12.10f)
+				curveToRelative(00f, -6.20f, -0.30f, -40.40f, -0.30f, -61.40f)
+				curveToRelative(00f, 00f, -700f, 150f, -84.70f, -29.80f)
+				curveToRelative(00f, 00f, -11.40f, -29.10f, -27.80f, -36.60f)
+				curveToRelative(00f, 00f, -22.90f, -15.70f, 1.60f, -15.40f)
+				curveToRelative(00f, 00f, 24.90f, 20f, 38.60f, 25.80f)
+				curveToRelative(21.90f, 38.60f, 58.60f, 27.50f, 72.90f, 20.90f)
+				curveToRelative(2.30f, -160f, 8.80f, -27.10f, 160f, -33.70f)
+				curveToRelative(-55.90f, -6.20f, -112.30f, -14.30f, -112.30f, -110.50f)
+				curveToRelative(00f, -27.50f, 7.60f, -41.30f, 23.60f, -58.90f)
+				curveToRelative(-2.60f, -6.50f, -11.10f, -33.30f, 2.60f, -67.90f)
+				curveToRelative(20.90f, -6.50f, 690f, 270f, 690f, 270f)
+				curveToRelative(200f, -5.60f, 41.50f, -8.50f, 62.80f, -8.50f)
+				reflectiveCurveToRelative(42.8f, 2.9f, 62.8f, 8.5f)
+				curveToRelative(00f, 00f, 48.10f, -33.60f, 690f, -270f)
+				curveToRelative(13.70f, 34.70f, 5.20f, 61.40f, 2.60f, 67.90f)
+				curveToRelative(160f, 17.70f, 25.80f, 31.50f, 25.80f, 58.90f)
+				curveToRelative(00f, 96.50f, -58.90f, 104.20f, -114.80f, 110.50f)
+				curveToRelative(9.20f, 7.90f, 170f, 22.90f, 170f, 46.40f)
+				curveToRelative(00f, 33.70f, -0.30f, 75.40f, -0.30f, 83.60f)
+				curveToRelative(00f, 6.50f, 4.60f, 14.40f, 17.30f, 12.10f)
+				curveTo(428.20f, 457.80f, 4960f, 362.90f, 4960f, 2520f)
+				curveTo(4960f, 113.30f, 383.50f, 80f, 244.80f, 80f)
+				close()
+				moveTo(97.2f, 352.9f)
+				curveToRelative(-1.30f, 10f, -10f, 3.30f, 0.70f, 5.20f)
+				curveToRelative(1.60f, 1.60f, 3.90f, 2.30f, 5.20f, 10f)
+				curveToRelative(1.30f, -10f, 10f, -3.30f, -0.70f, -5.20f)
+				curveToRelative(-1.60f, -1.60f, -3.90f, -2.30f, -5.20f, -10f)
+				close()
+				moveToRelative(-10.8f, -8.1f)
+				curveToRelative(-0.70f, 1.30f, 0.30f, 2.90f, 2.30f, 3.90f)
+				curveToRelative(1.60f, 10f, 3.60f, 0.70f, 4.30f, -0.70f)
+				curveToRelative(0.70f, -1.30f, -0.30f, -2.90f, -2.30f, -3.90f)
+				curveToRelative(-20f, -0.60f, -3.60f, -0.30f, -4.30f, 0.70f)
+				close()
+				moveToRelative(32.4f, 35.6f)
+				curveToRelative(-1.60f, 1.30f, -10f, 4.30f, 1.30f, 6.20f)
+				curveToRelative(2.30f, 2.30f, 5.20f, 2.60f, 6.50f, 10f)
+				curveToRelative(1.30f, -1.30f, 0.70f, -4.30f, -1.30f, -6.20f)
+				curveToRelative(-2.20f, -2.30f, -5.20f, -2.60f, -6.50f, -10f)
+				close()
+				moveToRelative(-11.4f, -14.7f)
+				curveToRelative(-1.60f, 10f, -1.60f, 3.60f, 00f, 5.90f)
+				curveToRelative(1.60f, 2.30f, 4.30f, 3.30f, 5.60f, 2.30f)
+				curveToRelative(1.60f, -1.30f, 1.60f, -3.90f, 00f, -6.20f)
+				curveToRelative(-1.40f, -2.30f, -40f, -3.30f, -5.60f, -20f)
+				close()
+			}
+		}.build()
+		return _GithubBrandsSolid!!
+	}
+
+private var _GithubBrandsSolid: ImageVector? = null
